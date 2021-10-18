c2cf0aaf7820e18e3b652e5d4d1d73448b5bf36b
atenasadat <atenasaghi@yahoo.com>
2021-10-18 10:49:03 +0330

    Subtraction  function added

--- operator.py
+++ operator.py
@@ -2,8 +2,13 @@
 
 def add(a , b):
     """
-    This function gets two numbers and returns their sum
+    This function gets two numbers and returns a+b
     """
     return a+b
 
 
+def sub(a , b):
+    """
+    This function gets two numbers and returns a - b
+    """
+    return a-b
